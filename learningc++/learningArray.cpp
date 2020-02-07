ca8388e39edefff77feb568b88359ba43893a81b
aweng126 <13075313106@163.com>
2020-02-07 17:44:05 +0800

    learningSet

--- learningc++/learningArray.cpp
+++ learningc++/learningArray.cpp
@@ -5,48 +5,48 @@
 //  Copyright © 2019 李庆文. All rights reserved.
 //  c++ 11 新添加的数据类型。
  
-#include <stdio.h>
-#include <iostream>
-#include <array>
-using namespace std;
-template<class T>
-void printArray(string s,const T& marr){
-    cout<<s<<'\t';
-    for(auto a:marr){
-        cout<<a<<'\t';
-    }
-    cout<<endl;
-}
-
-int main(){
-    //声明
-    array<int,3> marr = {3,2,4};
-    array<int,2> marr1{1,2};
-    printArray("marr",marr);
-    printArray("marr1 ",marr1);
-    
-    // access 相关
-    cout<<"marr[0]: "<<marr[0]<<endl;
-    cout<<"marr.at(0): "<<marr.at(0)<<endl;
-    cout<<"marr.data(): "<<*marr.data()<<endl;
-    cout<<"marr.front(): "<<marr.front()<<endl;
-    cout<<"marr.back(): "<<marr.back()<<endl;
-    
-    //capacity 相关
-    cout<<"marr.size(): "<<marr.size()<<endl;
-    cout<<"marr.empty(): "<<marr.empty()<<endl;
-    cout<<"marr.max_size(): "<<marr.max_size()<<endl;
-    
-    //Operations
-    printArray("before sort", marr);
-    sort(marr.begin(),marr.end());
-    printArray("after sort", marr);
-    
-    marr.fill(5);
-    printArray("marr.fill(5)", marr);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <iostream>
+//#include <array>
+//using namespace std;
+//template<class T>
+//void printArray(string s,const T& marr){
+//    cout<<s<<'\t';
+//    for(auto a:marr){
+//        cout<<a<<'\t';
+//    }
+//    cout<<endl;
+//}
+//
+//int main(){
+//    //声明
+//    array<int,3> marr = {3,2,4};
+//    array<int,2> marr1{1,2};
+//    printArray("marr",marr);
+//    printArray("marr1 ",marr1);
+//    
+//    // access 相关
+//    cout<<"marr[0]: "<<marr[0]<<endl;
+//    cout<<"marr.at(0): "<<marr.at(0)<<endl;
+//    cout<<"marr.data(): "<<*marr.data()<<endl;
+//    cout<<"marr.front(): "<<marr.front()<<endl;
+//    cout<<"marr.back(): "<<marr.back()<<endl;
+//    
+//    //capacity 相关
+//    cout<<"marr.size(): "<<marr.size()<<endl;
+//    cout<<"marr.empty(): "<<marr.empty()<<endl;
+//    cout<<"marr.max_size(): "<<marr.max_size()<<endl;
+//    
+//    //Operations
+//    printArray("before sort", marr);
+//    sort(marr.begin(),marr.end());
+//    printArray("after sort", marr);
+//    
+//    marr.fill(5);
+//    printArray("marr.fill(5)", marr);
+//
+//    return 0;
+//}
 
 /* 运行结果
  marr    3    2    4
--- learningc++/learningSet.cpp
+++ learningc++/learningSet.cpp
@@ -3,43 +3,119 @@
 //  learningc++
 //
 //  Created by 李庆文 on 2019/8/20.
+//  Updated by 李庆文 on 2020/2/7
 //  Copyright © 2019 李庆文. All rights reserved.
 //
 
 #include <stdio.h>
 #include <set>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
 
-//int main(){
-//    
-//    set<int> a;
-//    a.insert(1);
-//    cout<<"a.size()"<<a.size()<<endl;
-//    a.insert(1);
-//    cout<<"a.size()"<<a.size()<<endl;
-//    set<int> b={1,2,3};
-//    cout<<"b.size()"<<b.size()<<endl;
-//    cout<<"b.empty()\t"<<b.empty()<<endl;
-//    b.clear();
-//    cout<<"clear\t"<<b.size()<<endl;
-//    
-//    set<int> c(a);
-//    cout<<"c.size()"<<c.size()<<endl;
-//    c.insert(3);
-//    cout<<"c.end\t"<<*c.end()<<endl;
-//    cout<<"c.begin\t"<<*c.begin()<<endl;
-//    
-//    //find 会返回一个itertor
-//    auto f = c.find(2);
-//    if(f!=c.end()){
-//        cout<<"find \t"<<*f<<endl;
-//    }else{
-//        cout<<"not found 2"<<endl;
-//    }
-//    
-//    cout<<"c.find(2)\t"<<*c.find(3)<<endl;
-//    cout<<"c.count(3)"<<c.count(3)<<endl;
-//    
-//    return 0;
-//}
+template<class T>
+void printSet(const string s,const T& mset){
+    cout<<s<<'\t';
+    for(auto a:mset){
+        cout<<a<<'\t';
+    }
+    cout<<endl;
+}
+
+int main(){
+    // 声明
+    set<int> a;
+    set<int> a1 = {1,2,3};
+    set<int> a2(a1);
+    set<int> a3(a1.begin(),a1.begin());
+    
+    printSet("a:", a);
+    printSet("a1:",a1 );
+    printSet("a2:", a2);
+    printSet("a3:", a3);
+    
+    //capacity相关
+    cout<<"a.size(): "<<a.size()<<endl;
+    a.insert(1);
+    cout<<"after insert 1, a.size()"<<a.size()<<endl;
+    //set的特性就是不能插入重复元素,如果插入那么就会报错。
+    // a.insert(1);
+    //cout<<"a.size()"<<a.size()<<endl;
+    //printSet("after 2 time insert 1: ", a);
+    
+    cout<<"a.empty(): "<<a.empty()<<endl;
+    cout<<"a.max_size(): "<<a.max_size()<<endl;
+    
+    //modifiers
+    a.insert(3);
+    printSet("after insert 3: ", a);
+    
+    auto find3 = a.find(3);
+    a.erase(find3);
+    printSet("after erase3: ", a);
+
+    a.clear();
+    printSet("after clear:", a);
+    
+    a = {1,2,3,4,5};
+    // Look up相关
+    //find 会返回一个itertor
+    auto f = a.find(2);
+    if(f!=a.end()){
+        cout<<"find \t"<<*f<<endl;
+    }else{
+        cout<<"not found 2"<<endl;
+    }
+    cout<<"a.find(2)\t"<<*a.find(2)<<endl;
+    // count
+    cout<<"a.count(3): "<<a.count(3)<<endl;
+   
+    // equal_range
+    auto equal2 = a.equal_range(2);
+    for(auto i = equal2.first; i != equal2.second; i++){
+        cout<<"equal2: "<<*i<<endl;
+    }
+    
+    // 确认set中不会有重复元素，那么如果找到，则两个iterator之差肯定为1，
+    // 如果找不到，那么差为0,后来发现set的iterator也不可做差运算，此处存疑。
+
+    
+    // upper_bound 和 lower_bound
+    auto ub = a.upper_bound(4);
+    auto lb = a.lower_bound(2);
+    cout<<"lower_bound = 2, upper_bound = 4"<<endl;
+    for(auto i = lb; i!=ub; i++){
+        cout<<*i<<'\t';
+    }
+    cout<<endl;
+    
+    // 遍历操作
+    set<int>::iterator iter = a.begin(); // 等价于auto iter = a.begin();
+    for(;iter!=a.end();iter++){
+        cout<<*iter<<'\t';
+    }
+    
+    
+    return 0;
+}
+
+/* 运行结果
+ a:
+ a1:    1    2    3
+ a2:    1    2    3
+ a3:
+ a.size(): 0
+ after insert 1, a.size()1
+ a.empty(): 0
+ a.max_size(): 576460752303423487
+ after insert 3:     1    3
+ after erase3:     1
+ after clear:
+ find     2
+ a.find(2)    2
+ a.count(3): 1
+ equal2: 2
+ lower_bound = 2, upper_bound = 4
+ 2    3    4
+ 1    2    3    4    5    Program ended with exit code: 0
+ */
