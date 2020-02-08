2005ce0e0e8282caa1c35c67a3e4d0165a35a55d
aweng126 <13075313106@163.com>
2020-02-08 15:05:15 +0800

    update learningStack

--- learningc++/learningQueue.cpp
+++ learningc++/learningQueue.cpp
@@ -7,57 +7,57 @@
 //  Copyright © 2019 李庆文. All rights reserved.
 //
 
-#include <stdio.h>
-#include <queue>
-#include <iostream>
-using namespace std;
-
-int main(){
-    // 声明
-    queue<int> q ;
-    queue<int> q1({1,2,3,4});
-    queue<int> q2(q1);
-    
-    cout<<"q2: ";
-    while(q2.size() != 0){
-         cout<<q2.front()<<'\t';
-         q2.pop();
-     }
-    cout<<endl;
-    
-    // 其他操作。 如果队列为空，那么调用front和back的时候就会报错，
-    // 所以给我们的警示就是在通过front和back方法获取队列元素的时候，需要提前确定内部是含有对应的元素的。
-    //cout<<"q.front():"<<q.front()<<endl;
-    //cout<<"q.back():"<<q.back()<<endl;
-    cout<<"q.empty():"<<q.empty()<<endl;
-    cout<<"q.size():"<<q.size()<<endl;
-    
-    
-    q.push(1);
-    q.emplace(2);
-    
-    cout<<"after push q.front():"<<q.front()<<endl;
-    cout<<"after push q.back():"<<q.back()<<endl;
-    cout<<"after push q.empty():"<<q.empty()<<endl;
-    cout<<"after push q.size():"<<q.size()<<endl;
-    
-    
-    q.pop();
-    cout<<"after pop(): ";
-    while(q.size() != 0){
-        cout<<q.front()<<'\t';
-        q.pop();
-    }
-    return 0;
-}
-
-/* 运行结果
- q2: 1    2    3    4
- q.empty():1
- q.size():0
- after push q.front():1
- after push q.back():2
- after push q.empty():0
- after push q.size():2
- after pop(): 2    Program ended with exit code: 0
- */
+//#include <stdio.h>
+//#include <queue>
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//    // 声明
+//    queue<int> q ;
+//    queue<int> q1({1,2,3,4});
+//    queue<int> q2(q1);
+//
+//    cout<<"q2: ";
+//    while(q2.size() != 0){
+//         cout<<q2.front()<<'\t';
+//         q2.pop();
+//     }
+//    cout<<endl;
+//
+//    // 其他操作。 如果队列为空，那么调用front和back的时候就会报错，
+//    // 所以给我们的警示就是在通过front和back方法获取队列元素的时候，需要提前确定内部是含有对应的元素的。
+//    //cout<<"q.front():"<<q.front()<<endl;
+//    //cout<<"q.back():"<<q.back()<<endl;
+//    cout<<"q.empty():"<<q.empty()<<endl;
+//    cout<<"q.size():"<<q.size()<<endl;
+//
+//
+//    q.push(1);
+//    q.emplace(2);
+//
+//    cout<<"after push q.front():"<<q.front()<<endl;
+//    cout<<"after push q.back():"<<q.back()<<endl;
+//    cout<<"after push q.empty():"<<q.empty()<<endl;
+//    cout<<"after push q.size():"<<q.size()<<endl;
+//
+//
+//    q.pop();
+//    cout<<"after pop(): ";
+//    while(q.size() != 0){
+//        cout<<q.front()<<'\t';
+//        q.pop();
+//    }
+//    return 0;
+//}
+//
+///* 运行结果
+// q2: 1    2    3    4
+// q.empty():1
+// q.size():0
+// after push q.front():1
+// after push q.back():2
+// after push q.empty():0
+// after push q.size():2
+// after pop(): 2    Program ended with exit code: 0
+// */
--- learningc++/learningStack.cpp
+++ learningc++/learningStack.cpp
@@ -3,37 +3,65 @@
 //  learningc++
 //
 //  Created by 李庆文 on 2019/8/20.
+//  Updated by 李庆文 on 2019/2/8
 //  Copyright © 2019 李庆文. All rights reserved.
 //  Stack 栈 的使用：后进先出
 
 #include <stdio.h>
 #include <iostream>
 #include <stack>
-//using namespace std;
-//int main(){
-//    stack<int> s;
-//    cout<<"initial size"<<s.size()<<endl;
-//    s.push(1);
-//    cout<<"after push size\t"<<s.size()<<endl;
-//    cout<<" empty\t"<<s.empty()<<endl;
-//    cout<<"top element is\t"<<s.top()<<endl;
-//    s.push(2);
-//    s.push(3);
-//    s.push(4);
-//    cout<<"push 234, size \t"<<s.size()<<endl;
-//
-//    s.pop();
-//    cout<<"pop size\t"<<s.size()<<endl;
-//
-//    stack<int> s1(s);
-//    cout<<"s.size()\t"<<s.size()<<" s1.size()\t"<<s1.size()<<endl;
-//    auto len = s1.size();
-//    for(int i=0;i<len;i++){
-//        cout<<"i=\t"<<i<<"\t"<<s1.top()<<endl;
-//        s1.pop();
-//    }
-//
-//    cout<<"s.size"<<s.size()<<endl;
-//    cout<<"s1.size"<<s1.size()<<endl;
-//    return 0;
-//}
+using namespace std;
+int main(){
+    // 声明
+    stack<int> s;
+    stack<int> s1({1,2,3});
+    stack<int> s2(s1);
+    
+    cout<<"s2= ";
+     while(!s2.empty()){
+         cout<<s2.top()<<'\t';
+         s2.pop();
+     }
+    cout<<endl;
+    
+    //操作
+    cout<<"s.empty(): "<<s.empty()<<endl;
+    cout<<"s.size(): "<<s.size()<<endl;
+    // 这里也需要注意一下，必须在确认stack中元素个数不为0的情况下，
+    // 才可以使用s.top()来获取栈顶元素，否则就会报错。
+    //cout<<"s.top():"<<s.top()<<endl;
+    
+    s.push(1);
+    s.emplace(2);
+    cout<<"after push2 s.size(): "<<s.size()<<endl;
+    cout<<"after push2 s.empty(): "<<s.empty()<<endl;
+    cout<<"after push2 top element is\t"<<s.top()<<endl;
+
+    s.pop();
+    cout<<"after pop(),s.size():"<<s.size()<<endl;
+    cout<<"after pop(),s.top():"<<s.top()<<endl;
+
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    // 遍历
+    cout<<"after push 345,s= ";
+    while(!s.empty()){
+        cout<<s.top()<<'\t';
+        s.pop();
+    }
+    cout<<endl;
+    return 0;
+}
+/* 运行结果
+ s2= 3    2    1
+ s.empty(): 1
+ s.size(): 0
+ after push2 s.size(): 2
+ after push2 s.empty(): 0
+ after push2 top element is    2
+ after pop(),s.size():1
+ after pop(),s.top():1
+ after push 345,s= 5    4    3    1
+ Program ended with exit code: 0
+ */
